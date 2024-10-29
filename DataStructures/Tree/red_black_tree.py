393e790ecde535f3ece27de4d988baa27df6f9c3
jdguzmanc12 <jd.guzmanc12@uniandes.edu.co>
2024-10-29 17:24:03 -0500

    def inseRT

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -1,4 +1,7 @@
-from DataStructures.Tree import rbt_node
+from DataStructures.Tree import rbt_node as rbt_node
+
+RED = 0
+BLACK = 1
 
 def new_map():
     return {"root": None, "type": "RBT"}
@@ -16,7 +19,7 @@ def size_node(node):
 
 def put(my_rbt, key, value):
     my_rbt["root"] = insert_node(my_rbt["root"], key, value)
-    my_rbt["root"]["color"] = rbt_node.BLACK
+    my_rbt["root"]["color"] = BLACK
 
 def get(my_rbt, key):
     return get_node(my_rbt["root"], key)
@@ -27,15 +30,15 @@ def contains(my_rbt, key):
 def remove(my_rbt, key):
     if contains(my_rbt, key):
         if not rbt_node.is_red(my_rbt["root"]["left"]) and not rbt_node.is_red(my_rbt["root"]["right"]):
-            rbt_node.change_color(my_rbt["root"], rbt_node.RED)
+            rbt_node.change_color(my_rbt["root"], RED)
         my_rbt["root"] = remove_key(my_rbt["root"], key)
         if my_rbt["root"] is not None:
-            my_rbt["root"]["color"] = rbt_node.BLACK
+            my_rbt["root"]["color"] = BLACK
 
 def key_set(my_rbt):
     keys = []
     in_order_keys(my_rbt["root"], keys)
-    return keys
+    return {"size": len(keys), "elements": keys}
 
 def in_order_keys(node, keys):
     if node:
@@ -46,7 +49,7 @@ def in_order_keys(node, keys):
 def value_set(my_rbt):
     values = []
     in_order_values(my_rbt["root"], values)
-    return values
+    return {"size": len(values), "elements": values}
 
 def in_order_values(node, values):
     if node:
@@ -67,7 +70,7 @@ def rotate_left(node):
     node["right"] = right_node["left"]
     right_node["left"] = node
     right_node["color"] = node["color"]
-    node["color"] = rbt_node.RED
+    node["color"] = RED
     return right_node
 
 def rotate_right(node):
@@ -75,34 +78,37 @@ def rotate_right(node):
     node["left"] = left_node["right"]
     left_node["right"] = node
     left_node["color"] = node["color"]
-    node["color"] = rbt_node.RED
+    node["color"] = RED
     return left_node
 
 def flip_colors(node):
-    node["color"] = rbt_node.RED if node["color"] == rbt_node.BLACK else rbt_node.BLACK
+    node["color"] = RED if node["color"] == BLACK else BLACK
     if node["left"]:
-        node["left"]["color"] = rbt_node.BLACK if node["left"]["color"] == rbt_node.RED else rbt_node.RED
+        node["left"]["color"] = BLACK if node["left"]["color"] == RED else RED
     if node["right"]:
-        node["right"]["color"] = rbt_node.BLACK if node["right"]["color"] == rbt_node.RED else rbt_node.RED
+        node["right"]["color"] = BLACK if node["right"]["color"] == RED else RED
 
 def insert_node(node, key, value):
     if node is None:
-        return rbt_node.new_node(key, value, rbt_node.RED)
-    if key < node["key"]:
-        node["left"] = insert_node(node["left"], key, value)
-    elif key > node["key"]:
-        node["right"] = insert_node(node["right"], key, value)
-    else:
+        return rbt_node.new_node(key, value)
+    
+    if key == node["key"]:
         node["value"] = value
-
-    if rbt_node.is_red(node["right"]) and not rbt_node.is_red(node["left"]):
+    elif key < node["key"]:
+        node["left"] = insert_node(node["left"], key, value)
+    else:
+        node["right"] = insert_node(node["right"], key, value)
+    if node["right"] is not None and rbt_node.is_red(node["right"]) and (node["left"] is None or not rbt_node.is_red(node["left"])):
         node = rotate_left(node)
-    if rbt_node.is_red(node["left"]) and rbt_node.is_red(node["left"]["left"]):
+    if node["left"] is not None and rbt_node.is_red(node["left"]) and node["left"]["left"] is not None and rbt_node.is_red(node["left"]["left"]):
         node = rotate_right(node)
-    if rbt_node.is_red(node["left"]) and rbt_node.is_red(node["right"]):
+    if node["left"] is not None and rbt_node.is_red(node["left"]) and node["right"] is not None and rbt_node.is_red(node["right"]):
         flip_colors(node)
-
+    
     return node
+
+
+
 
 def get_node(node, key):
     while node is not None:
@@ -114,45 +120,56 @@ def get_node(node, key):
             return node["value"]
     return None
 
+def floor(my_rbt, key):
+    return floor_node(my_rbt["root"], key)
 
-def floor(node, key):
+def floor_node(node, key):
     if node is None:
         return None
     if key == node["key"]:
         return node["key"]
     if key < node["key"]:
-        return floor(node["left"], key)
-    t = floor(node["right"], key)
+        return floor_node(node["left"], key)
+    t = floor_node(node["right"], key)
     return t if t is not None else node["key"]
 
-def ceiling(node, key):
+def ceiling(my_rbt, key):
+    return ceiling_node(my_rbt["root"], key)
+
+def ceiling_node(node, key):
     if node is None:
         return None
     if key == node["key"]:
         return node["key"]
     if key > node["key"]:
-        return ceiling(node["right"], key)
-    t = ceiling(node["left"], key)
+        return ceiling_node(node["right"], key)
+    t = ceiling_node(node["left"], key)
     return t if t is not None else node["key"]
 
-def select(node, k):
+def select(my_rbt, k):
+    return select_node(my_rbt["root"], k)
+
+def select_node(node, k):
     if node is None:
         return None
     t = size_node(node["left"])
     if t > k:
-        return select(node["left"], k)
+        return select_node(node["left"], k)
     elif t < k:
-        return select(node["right"], k - t - 1)
+        return select_node(node["right"], k - t - 1)
     else:
         return node["key"]
 
-def rank(node, key):
+def rank(my_rbt, key):
+    return rank_node(my_rbt["root"], key)
+
+def rank_node(node, key):
     if node is None:
         return 0
     if key < node["key"]:
-        return rank(node["left"], key)
+        return rank_node(node["left"], key)
     elif key > node["key"]:
-        return 1 + size_node(node["left"]) + rank(node["right"], key)
+        return 1 + size_node(node["left"]) + rank_node(node["right"], key)
     else:
         return size_node(node["left"])
 
@@ -201,4 +218,3 @@ def collect_values(node, result, low, high):
         result.append(node["value"])
     if high > node["key"]:
         collect_values(node["right"], result, low, high)
-
